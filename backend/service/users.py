3beded57a6312609e577cd939acfcd725c0599fc
yuukich1 <ultimapgg@gmail.com>
2024-10-21 21:17:34 +0400

    fix update pasword

--- backend/service/users.py
+++ backend/service/users.py
@@ -1,7 +1,8 @@
+from fastapi import HTTPException
 from schemas.users import UserCreate, User, UserLogin, UserUpdate
 from dependencies import UserRepository
-
-
+from passlib.hash import pbkdf2_sha256
+from utils.enums import AuthStatus
 class UserService: 
 
     def __init__(self, user_repository: UserRepository):
@@ -17,6 +18,11 @@ class UserService:
     
     def update_user(self, user_id: int, data: UserUpdate):
         entity = data.model_dump()
+        user = self.get_user_filter_by(id=user_id)
+        if data.password and not pbkdf2_sha256.verify(data.password, user.password):
+            raise HTTPException(status_code=403, detail={'status': AuthStatus.INVALID_PASSWORD.value})
+        if data.password:
+            entity['password'] = pbkdf2_sha256.hash(data.password)
         entity['id'] = user_id
         entity = {k: v for k, v in entity.items() if v is not None}
         return self.user_repository.update(entity)
